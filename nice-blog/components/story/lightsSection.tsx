f7ca460fa212ea45d16d4b1c3ee94bd0ec0228a3
Espen Halvorsen <espen.halvorsen@gmail.com>
2021-05-04 10:00:14 +0200

    wip: Lights on blog.

--- nice-blog/components/story/lightsSection.tsx
+++ nice-blog/components/story/lightsSection.tsx
@@ -13,18 +13,18 @@ export const LightsSection: React.FunctionComponent = () => {
     <section>
       <SectionHeader>Lights</SectionHeader>
       <StoryParagraph>
-        This year our Christmas tree flexed 400 Twinkly Special Edition LEDs.
-        They're chained in two strands, 400 LEDs in total, programmable using
-        the Twinkly app. The LEDs are made for both indoor and outdoor use,
-        rated IP44 (splash proof) and can withstand temperatures down to
-        <Degrees degreesCelsius={-15} />.
-        <a
+        Last year our Christmas tree donned 400 individually programmable lights, specifically the wonderful <a
           href={
             "https://www.twinkly.com/products/strings-special-edition-400-leds/"
           }
         >
-          Spec
-        </a>
+          Twinkly Special Edition LEDs
+        </a>. A big hit with three-fourths of the household, these LEDs can be configured using an app to form thousands of equally classy Holiday themes. You can also use animations, built-in or custom, and these endlessly loop  LEDs. The included power brick even includes a microphone allowing the lights to react to sounds, flashing brightly with every ho-ho-ho detected.
+        They're chained in two strands, 400 LEDs in total, programmable using
+        the Twinkly app. The LEDs are made for both indoor and outdoor use,
+        rated IP44 (splash proof) and can withstand temperatures down to
+        <Degrees degreesCelsius={-15} />.
+
       </StoryParagraph>
       <StoryParagraph>
         The lights were a big success with three-fourths of the household, and
